36be889cd3eb0c43f0e2c7193cc29afd107b46a8
Alfie <39995471+alfred9292@users.noreply.github.com>
2021-06-29 14:47:01 +1000

    Update logistic regression.py

--- logistic regression.py
+++ logistic regression.py
@@ -8,17 +8,20 @@ import time
 
 start =time.time()
 
+#create spark session
 spark = SparkSession \
     .builder \
     .appName("logistic regression") \
     .getOrCreate()
 
+# read training and testing data
 train_datafile = "/Users/alfred/PycharmProjects/cc2/data/Train-label-28x28.csv"
 
 test_datafile = "/Users/alfred/PycharmProjects/cc2/data/Test-label-28x28.csv"
 train_data = spark.read.csv(train_datafile, header=False,inferSchema="true")
 test_data = spark.read.csv(test_datafile, header=False,inferSchema="true")
 
+# extract features and convert to vectors.
 assembler = VectorAssembler(inputCols=train_data.columns[1:],outputCol="features")
 assembler_test = VectorAssembler(inputCols=test_data.columns[1:],outputCol="features")
 
@@ -27,6 +30,7 @@ test_vectors = assembler_test.transform(test_data).select(test_data.columns[0],"
 
 train_vectors.show(2)
 
+# use PCA reduce dimension to 75.
 pca = PCA(k=75, inputCol="features",outputCol='pca')
 
 model = pca.fit(train_vectors)
@@ -35,13 +39,16 @@ pca_result = model.transform(train_vectors).select(train_vectors.columns[0],'pca
 
 pca_testresult = model.transform(test_vectors).select(test_vectors.columns[0],'pca')
 
+#transform result to rdd.
 rdd_train = pca_result.rdd
 rdd_test = pca_testresult.rdd
 
+# function to convert rdd to labelled point to fit the required format for logistic regression with LBFGS.
 def convert(y):
     d = [x for x in y]
     return LabeledPoint(d[0],d[1:])
 
+# apply convert function to all features stored in rdd by using lambda function. 
 parsed_traindata = rdd_train.map(lambda x:convert(x))
 
 parsed_testdata = rdd_test.map(lambda x:convert(x))
@@ -55,6 +62,7 @@ classifier = LogisticRegressionWithLBFGS()
 
 model1 = classifier.train(parsed_traindata,numClasses=10)
 
+# Using lambda function to make prediction. 
 prediciton = parsed_testdata.map(lambda p:(float(model1.predict(p.features)),p.label))
 
 end = time.time()
@@ -63,10 +71,13 @@ print("Execution time = %s" % time)
 p=prediciton.take(20)
 print(p)
 
+# Using lambda function to calculate accurancy. 
+
 accuracy = prediciton.filter(lambda lp:lp[0] ==lp[1]).count() /float(parsed_testdata.count())
 
 metrics = MulticlassMetrics(prediciton)
 
+# performance metrics calculation.
 confusion_matrix= metrics.confusionMatrix()
 precision = metrics.precision()
 recall = metrics.recall()
